984d3773b0acc811581ea922d2e250593423330a
gnehs <jayuiop@gmail.com>
2022-06-02 19:08:16 +0800

    migrate to d3

--- data-fetcher/index.js
+++ data-fetcher/index.js
@@ -1,28 +1,109 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+async function downloadAllData() {
+  const { data: main } = await axios.get('https://gnehs.github.io/ntut-course-crawler-node/main.json')
+  let parsedMain = Object.entries(main).map(([key, value]) => [...value.map(x => `${key}/${x}`)]).flat()
+  let res = { main: parsedMain }
+  let done = 0
+  let tasks = parsedMain.map(async x => {
+    const { data } = await axios.get(`https://gnehs.github.io/ntut-course-crawler-node/${x}/main.json`)
+    res[x] = data
+    done++
+    console.log(`(${done}/${parsedMain.length})`, x)
+  })
+  await Promise.all(tasks)
+  return res
+}
+
+
 (async function () {
-  const { data: main } = await axios.get('https://gnehs.github.io/ntut-course-crawler-node/main.json');
+  const data = await downloadAllData()
+
+  // withdraw_teacher
+  let teachers = {}
   let result = {};
-  result[Object.keys(main)[0]] = {}
-  for (let year of Object.keys(main)) {
-    let courses = []
-    result[year] = structuredClone(result[year - 1] ?? {})
-    console.log(year, Math.max(...Object.values(result[year])))
-    for (let sem of main[year]) {
-      console.log('[fetch]', year, sem);
-      const courseData = await axios.get(`https://gnehs.github.io/ntut-course-crawler-node/${year}/${sem}/main.json`);
-      courses = courses.concat(courseData.data);
-    }
-    for (let course of courses) {
+  for (let yearsem of data.main) {
+    const courseData = data[yearsem];
+    for (let course of courseData) {
       for (let teacher of course.teacher) {
         let name = teacher.name
-        if (!result[year][name]) {
-          result[year][name] = 0
+        if (!teachers[name]) {
+          teachers[name] = 0
         }
-        result[year][name] += parseInt(course.peopleWithdraw)
+        teachers[name] += parseInt(course.peopleWithdraw)
       }
     }
+    let [year, sem] = yearsem.split('/')
+    // write data
+    result[`${parseInt(year) + 1911}-${sem == 1 ? '02' : '09'}-01`] = structuredClone(teachers)
   }
-  fs.writeFileSync(path.join(__dirname, '../result.json'), JSON.stringify(result));
+
+  // convert to csv
+  let csv = 'date,name,peopleWithdraw\n'
+  for (let date of Object.keys(result)) {
+    let data = result[date]
+    for (let teacher of Object.keys(data)) {
+      csv += `${date},${teacher},${data[teacher]}\n`
+    }
+  }
+  fs.writeFileSync(path.join(__dirname, '../datasets/withdraw_teacher.csv'), csv)
+
+  // withdraw_course
+  let courses = {}
+  result = {}
+  for (let yearsem of data.main) {
+    const courseData = data[yearsem];
+    for (let course of courseData) {
+      // remove () （）
+      let name = course.name.zh.replace(/\(.*\)|（.*）/g, '')
+      if (!courses[name]) {
+        courses[name] = 0
+      }
+      courses[name] += parseInt(course.peopleWithdraw)
+    }
+    let [year, sem] = yearsem.split('/')
+    // write data
+    result[`${parseInt(year) + 1911}-${sem == 1 ? '02' : '09'}-01`] = structuredClone(courses)
+  }
+  csv = 'date,name,peopleWithdraw\n'
+  for (let date of Object.keys(result)) {
+    let data = result[date]
+    for (let course of Object.keys(data)) {
+      csv += `${date},${course},${data[course]}\n`
+    }
+  }
+  fs.writeFileSync(path.join(__dirname, '../datasets/withdraw_course.csv'), csv)
+
+  // withdraw_general
+  courses = {}
+  result = {}
+  for (let yearsem of data.main) {
+    const courseData = data[yearsem].filter(x =>
+      x.class
+        .map(y => y.name)
+        .join('')
+        .match(/^博雅/)
+    )
+    for (let course of courseData) {
+      // remove () （）
+      let name = course.name.zh.replace(/\(.*\)|（.*）/g, '')
+      if (!courses[name]) {
+        courses[name] = 0
+      }
+      courses[name] += parseInt(course.peopleWithdraw)
+    }
+    let [year, sem] = yearsem.split('/')
+    // write data
+    result[`${parseInt(year) + 1911}-${sem == 1 ? '02' : '09'}-01`] = structuredClone(courses)
+  }
+  csv = 'date,name,peopleWithdraw\n'
+  for (let date of Object.keys(result)) {
+    let data = result[date]
+    for (let course of Object.keys(data)) {
+      csv += `${date},${course},${data[course]}\n`
+    }
+  }
+  fs.writeFileSync(path.join(__dirname, '../datasets/withdraw_general.csv'), csv)
+
 })()
